5bc67fdf092c3d6a1bbfc50e26e9a7687d986804
Felipedialvarenga <felipedialvarenga07@gmail.com>
2022-06-14 18:14:23 -0300

    feat: add initial api structure

---
+++ api/models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const Joi = require('joi')
+const passwordComplexity = require("joi-password-complexity")
+
+const userSchema = new mongoose.Schema({
+    name: {type:String, required:true},
+    email: {type:String, required:true},
+    password: {type:String, required}
+})
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({_id:this._id},process.env.JWTPRIVATEKEY, {expiresIn: '7d'});
+    return token
+}
+
+const User = mongoose.model("user",userSchema)
+
+const validate = (data) => {
+    const schema = Joi.object({
+        firstName: Joi.string().required().label("First Name"),
+        lastName: Joi.string().required().label("Last Name"),
+        email: Joi.string().email().required().label("Email"),
+        password: passwordComplexity.required().label("First Name"),
+    });
+    return schema.validate(data)
+}
+
+module.exports = {User, validate}
