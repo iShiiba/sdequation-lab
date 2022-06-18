d8f989a148b3a7680e70ba7e014ff41cf0a94c2f
Felipedialvarenga <felipedialvarenga07@gmail.com>
2022-06-18 12:19:28 -0300

    feat: add login route

--- api/app.js
+++ api/app.js
@@ -12,8 +12,10 @@ app.use(express.json())
 app.use(cors())
 
 const userRoute = require('./routes/users')
+const authRoute = require('./routes/auth')
 
 app.use('/users',userRoute)
+app.use('/auth',authRoute)
 
 const port = process.env.PORT || 8080
 app.listen(port, console.log(`Listening on port ${port}...`))
