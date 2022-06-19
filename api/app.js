45d20e767eedbdc19e32aa5b5d00010c5952d247
Felipedialvarenga <felipedialvarenga07@gmail.com>
2022-06-19 20:09:40 -0300

    feat: add documentation page

--- api/app.js
+++ api/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const connection = require("./db")
+const router = express.Router();
+const path = require('path')
 
 //database connection
 connection();
@@ -16,10 +18,15 @@ const authRoute = require('./routes/auth')
 const exerciseRoute = require('./routes/exercises')
 const summaryRoute = require('./routes/summary')
 
+app.get("/doc", (req,res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
 app.use('/users',userRoute)
 app.use('/auth',authRoute)
 app.use('/exercises',exerciseRoute)
 app.use('/summary',summaryRoute)
+
 
 const port = process.env.PORT || 8080
 app.listen(port, console.log(`Listening on port ${port}...`))
